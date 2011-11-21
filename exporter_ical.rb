a484d1243728197fe78719e88eb67ba110a7add0
Gillis Van Ginderachter <gvginder@vub.ac.be>
2011-11-21 23:44:36 +0100

    Fixed daylight saving time?

--- exporter_ical.rb
+++ exporter_ical.rb
@@ -10,8 +10,10 @@ class ICalExporter
 			event.summary = entry.course.title
 			event.description = entry.course.teacher
 			event.location = entry.course.location
-			event.dtstart = Time.parse("%04d-%02d-%02dT%02d:%02d:00+02:00" % [entry.start_date.year, entry.start_date.month, entry.start_date.day, entry.start_date.hour, entry.start_date.min]).getutc
-			event.dtend = Time.parse("%04d-%02d-%02dT%02d:%02d:00+02:00" % [entry.end_date.year, entry.end_date.month, entry.end_date.day, entry.end_date.hour, entry.end_date.min]).getutc
+			event.dtstart = Time.local(entry.start_date.year, entry.start_date.month, entry.start_date.day, entry.start_date.hour, entry.start_date.min).getutc
+			event.dtend = Time.local(entry.start_date.year, entry.start_date.month, entry.start_date.day, entry.start_date.hour, entry.start_date.min).getutc
+			#event.dtstart = Time.parse("%04d-%02d-%02dT%02d:%02d:00+02:00" % [entry.start_date.year, entry.start_date.month, entry.start_date.day, entry.start_date.hour, entry.start_date.min]).getutc
+			#event.dtend = Time.parse("%04d-%02d-%02dT%02d:%02d:00+02:00" % [entry.end_date.year, entry.end_date.month, entry.end_date.day, entry.end_date.hour, entry.end_date.min]).getutc
 			calendar.add_subcomponent(event)
 		end
 		calendar.to_s
